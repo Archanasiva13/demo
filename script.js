d2483978b9f5a663089112fae50f06fa1e72d4dc
Archanasiva13 <107271883+Archanasiva13@users.noreply.github.com>
2022-08-04 21:39:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-const body = document.body
-const slides = document.querySelectorAll('.slide')
+const imgs = document.getElementById('imgs')
 const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
 
-let activeSlide = 0
+const img = document.querySelectorAll('#imgs img')
+
+let idx = 0
+
+let interval = setInterval(run, 3500)
+
+function run() {
+    idx++
+    changeImage()
+}
+
+function changeImage() {
+    if(idx > img.length - 1) {
+        idx = 0
+    } else if(idx < 0) {
+        idx = img.length - 1
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`
+}
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
+}
 
 rightBtn.addEventListener('click', () => {
-  activeSlide++
-
-  if (activeSlide > slides.length - 1) {
-    activeSlide = 0
-  }
-
-  setBgToBody()
-  setActiveSlide()
+    idx++
+    changeImage()
+    resetInterval()
 })
 
 leftBtn.addEventListener('click', () => {
-  activeSlide--
-
-  if (activeSlide < 0) {
-    activeSlide = slides.length - 1
-  }
-
-  setBgToBody()
-  setActiveSlide()
+    idx--
+    changeImage()
+    resetInterval()
 })
-
-setBgToBody()
-
-function setBgToBody() {
-  body.style.backgroundImage = slides[activeSlide].style.backgroundImage
-}
-
-function setActiveSlide() {
-  slides.forEach((slide) => slide.classList.remove('active'))
-
-  slides[activeSlide].classList.add('active')
-}
-Footer
